3e18a08adaa3879ef4070f5750f620a9a4d277d5
Lucasz-py <escobarlucas27.10@gmail.com>
2025-09-01 19:31:10 -0300

    Update 01/09

--- Tp N°3/Circulo.java
+++ Tp N°3/Circulo.java
@@ -1,4 +1,3 @@
-
 /**
  * Class Circulo.
  * 
@@ -8,12 +7,18 @@
 public class Circulo{
     private double radio;
     private Punto centro;
-    
+     /**
+     * Constructor de Circulo
+     * @param p_radio asigna el valor del radio.
+     * @param p_centro asigna el obejo de clase Punto.
+     */
     public Circulo(double p_radio, Punto p_centro){
         this.setRadio(p_radio);
         this.setCentro(p_centro);
     }
-    
+     /**
+     * Constructor de Circulo sin parametro.
+     */
     public Circulo(){
         this.setRadio(0);
         this.setCentro(new Punto(0, 0));
@@ -22,7 +27,6 @@ public class Circulo{
     private void setRadio(double p_radio){
         this.radio = p_radio;
     }
-    
     private void setCentro(Punto p_centro){
         this.centro  = p_centro;
     }
@@ -30,34 +34,55 @@ public class Circulo{
     public double getRadio(){
         return radio;
     }
-    
     public Punto getCentro(){
         return centro;
     }
-    
+    /**
+    * Método desplazar
+    * @param p_dx valor a desplazar en x.
+    * @param p_dy valor a desplazar en y.
+    * desplza el rectangulo segun los valores de los parametros.
+    */
     public void desplazar(double p_dx, double p_dy){
         this.getCentro().desplazar(p_dx, p_dy);
     }
-    
+    /**
+    * Método perimetro
+    * Devuelve el resultado de calcular el perimetro del circulo.
+    */     
     public double perimetro(){
         return 2 * Math.PI * this.getRadio();
     }
-    
+    /**
+    * Método superficie
+    * Devuelve el resultado de calcular el superficie del circulo.
+    */ 
     public double superficie(){
         return Math.PI * Math.pow(this.getRadio(), 2);
     }
-    
+    /**
+    * Método perimetro
+    * Imprime en pantalla el circulo, su centro, radio, perimtro y superficie.
+    */
     public void caracteristicas(){
         System.out.println("******Circulo******");
         System.out.println("Centro: " + this.getCentro().coordenadas() + " -Radio: " + this.getRadio());
         System.out.println("Superficie: " + this.superficie() + " -Perimetro: " + this.perimetro());
         System.out.print("\n");
     }
-    
+    /**
+    * Método distanciaA
+    * @param otroCirculo objeto de clase Circulo.
+    * devuelve cuanto es la distania entre dos objetos de clase Ciruclo.
+    */
     public double distanciaA(Circulo otroCirculo){
         return this.getCentro().distanciaA(otroCirculo.getCentro());
     }
-    
+    /**
+    * Método elMayor
+    * @param otroCirculo objeto de clase Circulo.
+    * Compara dos objetos de Clase Circulo y devuelve el mayor.
+    */
     public Circulo elMayor(Circulo otroCirculo){
         if(this.superficie() >= otroCirculo.superficie()){
             return this;
--- Tp N°3/Rectangulo.java
+++ Tp N°3/Rectangulo.java
@@ -1,4 +1,3 @@
-
 /**
  * Class Rectangulo.
  * 
@@ -37,7 +36,6 @@ public class Rectangulo{
     private void setAncho(double p_ancho){
         this.ancho = p_ancho;
     }
-    
     private void setAlto(double p_alto){
         this.alto = p_alto;
     }
@@ -45,38 +43,58 @@ public class Rectangulo{
     public Punto getOrigen(){
         return origen;
     }
-    
     public double getAncho(){
         return ancho;
     }
-    
     public double getAlto(){
         return alto;
     }
-    
+    /**
+    * Método desplazar
+    * @param p_dx valor a desplazar en x.
+    * @param p_dy valor a desplazar en y.
+    * desplza el rectangulo segun los valores de los parametros.
+    */
     public void desplazar(double p_dx, double p_dy){
         this.getOrigen().desplazar(p_dx, p_dy);
     }
-    
+    /**
+    * Método perimetro
+    * Devuelve el resultado de calcular el perimetro del rectangulo.
+    */ 
     public double perimetro(){
         return 2 * (this.getAncho() + this.getAlto());
     }
-    
+     /**
+    * Método superficie
+    * Devuelve el resultado de calcular la superficie del rectangulo.
+    */
     public double superficie(){
         return this.getAncho() * this.getAlto();
     }
-    
+    /**
+    * Método perimetro
+    * Imprime en pantalla el rectangulo, su origen, alto, ancho, perimtro y superficie.
+    */
     public void caracteristicas(){
         System.out.println("******Rectangulo******");
         System.out.println("Origen: " + this.getOrigen().coordenadas() + " - Alto: " + this.getAlto() + " - Ancho: " + this.getAncho());
         System.out.println("Superficie: " + this.superficie() + " - Perimetro: " + this.perimetro());
         System.out.print("\n");
     }
-    
+    /**
+    * Método distanciaA
+    * @param otroRectangulo objeto de clase Rectangulo.
+    * devuelve cuanto es la distania entre dos objetos de clase Rectangulo.
+    */
     public double distanciaA(Rectangulo otroRectangulo){
         return this.getOrigen().distanciaA(otroRectangulo.getOrigen());
     }
-    
+    /**
+    * Método elMayor
+    * @param otroRectangulo objeto de clase Rectangulo.
+    * Compara dos objetos de Clase Rectangulo y devuelve el mayor.
+    */
     public Rectangulo elMayor(Rectangulo otroRectangulo){
         if (this.superficie() >= otroRectangulo.superficie()){
             return this;            
