512452d283fb91e971e82812049410d65c07f0a1
Lucasz-py <escobarlucas27.10@gmail.com>
2025-09-04 21:11:19 -0300

    Update 04/09

---
+++ Tp N°4/ArrayDePuntos.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+/**
+ * Write a description of class ArrayDePuntos here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ArrayDePuntos{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        
+        
+    }
+}
