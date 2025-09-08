e6a83615874af6d699d1bc7bda2c1d5afb481ecf
Lucasz-27 <escobarlucas27.10@gmail.com>
2025-09-08 15:03:32 -0300

    update 08/09

--- Tp N°4/ArrayDePuntos.java
+++ Tp N°4/ArrayDePuntos.java
@@ -9,6 +9,30 @@ public class ArrayDePuntos{
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
         
+        Punto[] puntos = new Punto[6];
         
+        for (int i = 0; i < puntos.length; i++) {
+            System.out.println("Ingrese coordenadas del punto " + (i + 1) + ": ");
+            System.out.print("X: ");
+            double x = scanner.nextDouble();
+            System.out.print("Y: ");
+            double y = scanner.nextDouble();
+            puntos[i] = new Punto(x, y);
+        }
+        
+        System.out.println("\n--- Coordenadas de los puntos ---");
+        int contador = 1;
+        for (Punto p : puntos) {
+            System.out.println("Punto " + contador + ": " + p.coordenadas());
+            contador++;
+        }
+        
+        System.out.println("\n--- Distancias entre puntos consecutivos ---");
+        for (int i = 0; i < puntos.length - 1; i++) {
+            double distancia = puntos[i].distanciaA(puntos[i + 1]);
+            System.out.println("Distancia p" + (i + 1) + "-p" + (i + 2) + ": " + distancia);
+        }
     }
 }
+
+    
