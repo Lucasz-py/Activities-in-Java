a3221eee93b40517b115115ccdcf950f0f5adb43
Lucasz-27 <escobarlucas27.10@gmail.com>
2025-09-20 19:37:48 -0300

    update Banco

--- Tp N°4/AplicacionBanco.java
+++ Tp N°4/AplicacionBanco.java
@@ -23,17 +23,19 @@ public class AplicacionBanco{
         System.out.println("Provincia: ");
         String nombreProvincia = scanner.next();
         //Crea localidad
-        Localidad localidadEmpleado = new Localidad(nombreLocalidad, nombreProvincia);
+        Localidad localidad = new Localidad(nombreLocalidad, nombreProvincia);
         //Crea el objeto Mibanco
-        Banco miBanco = new Banco(nombreBanco, localidadEmpleado,numSucursal, new ArrayList<Empleado>());
+        Banco miBanco = new Banco(nombreBanco, localidad, numSucursal, new ArrayList<Empleado>(), new ArrayList<CuentaBancaria>());
         //Banco miBanco = new Banco(nombreBanco, numSucursal, new ArrayList<Empleado>());
         
         do{
             System.out.println("\n---Menu----");
-            System.out.println("1. Contratar empleado ");
-            System.out.println("2. Mostrar lista de empleados ");
-            System.out.println("3. Mostrar ");
-            System.out.println("4. Despedir empleado");
+            System.out.println(">> 1. Contratar empleado ");
+            System.out.println(">> 2. Despedir empleado");
+            System.out.println(">> 3. Mostrar lista de empleados ");
+            System.out.println(">> 4. Mostrar ");
+            System.out.println(">> 5. Agregar cuenta Bancaria ");
+            System.out.println(">> 6. Mostrar Resumen de cuentas bancarias: ");
             System.out.println("0. Salir ");
             System.out.println("Seleccione una opcion: ");
             opcion = scanner.nextInt();
@@ -46,8 +48,7 @@ public class AplicacionBanco{
                     System.out.println("Opcion invalida");
                 case 1:
                 //Agregar empleado
-                System.out.println("--Agregar Empleado--");
-                System.out.println("\t");
+                System.out.println("--Agregar Empleado--\n");
                 System.out.println("Cuil: ");
                 long cuil = scanner.nextLong();
                 System.out.println("Apellido: ");
@@ -62,18 +63,17 @@ public class AplicacionBanco{
                 Empleado empleado = new Empleado(cuil, ape, nom, sueldoBasico, anio);
                 
                 miBanco.agregarEmpleado(empleado);
-                
-                break;
-                
-                case 2:
-                miBanco.listarSueldos();
                 break;
                 
                 case 3:
-                miBanco.mostrar();
+                miBanco.listarSueldos();
                 break;
                 
                 case 4:
+                miBanco.mostrar();
+                break;
+                
+                case 2:
                 System.out.println("--Despedir Empleado--");
                 System.out.println("Ingrese el CUIL del empleado a despedir: ");
                 long cuilEliminar = scanner.nextLong();
@@ -96,8 +96,37 @@ public class AplicacionBanco{
                 } else {
                     System.out.println("No se encontró ningún empleado con ese CUIL.");
                 }
-            break;        
+                break;  
+                
+                case 5:
+                    System.out.println("--Agregar Cuenta Bancaria--\n");
+                    System.out.println("Dni del titular:");
+                    int dniT = scanner.nextInt();
+                    System.out.println("Nombre del titular: ");
+                    String nomT = scanner.next();
+                    System.out.println("Apellido del titular: ");
+                    String apeT = scanner.next();
+                    System.out.println("Año de nacimiento del titular: ");
+                    int anioT = scanner.nextInt();
+                    Persona titular = new Persona(dniT, nomT, apeT, anioT);
+                    
+                    System.out.println("Nro de Cuenta: ");
+                    int nroCu = scanner.nextInt();
+                    System.out.println("Saldo: ");
+                    double saldoCB = scanner.nextDouble();
+                    CuentaBancaria acount = new CuentaBancaria(nroCu, titular, saldoCB);
+                    
+                    miBanco.agregarCuentaBancaria(acount);
+                break;
+                
+                case 6:
+                    miBanco.mostrarResumen();    
+                break;
             }
         }while(opcion != 0);
+        
+        //Ejercicio 4
+            
+        
     }
 }
--- Tp N°4/Banco.java
+++ Tp N°4/Banco.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 /**
  * Write a description of class Banco here.
  * 
@@ -10,6 +11,7 @@ public class Banco{
     private int nroSucursal;
     private Localidad localidad;
     private ArrayList<Empleado> empleado;
+    private ArrayList<CuentaBancaria> cuentas;
     
     public Banco(String p_nombre, Localidad p_localidad, int p_nroSucursal, Empleado p_empleado){
         this.setNombre(p_nombre);
@@ -25,6 +27,14 @@ public class Banco{
         this.setEmpleado(p_empleado);
     }
     
+    public Banco(String p_nombre, Localidad p_localidad, int p_nroSucursal, ArrayList<Empleado> p_empleado, ArrayList<CuentaBancaria> p_cuentas){
+        this.setNombre(p_nombre);
+        this.setLocalidad(p_localidad);
+        this.setNroSucursal(p_nroSucursal);
+        this.setEmpleado(p_empleado);
+        this.setCuentaBancaria(p_cuentas);
+    }
+    
     private void setNombre(String p_nombre){
         this.nombre = p_nombre;
     }
@@ -36,7 +46,11 @@ public class Banco{
     }
     private void setEmpleado(ArrayList<Empleado> p_empleado){
         this.empleado = p_empleado;
+    } 
+    private void setCuentaBancaria(ArrayList<CuentaBancaria> p_cuentas){
+        this.cuentas = p_cuentas;
     }
+    
     public String getNombre(){
         return this.nombre;
     }
@@ -48,6 +62,9 @@ public class Banco{
     }
     public ArrayList<Empleado> getEmpleado(){
         return this.empleado;
+    }
+    public ArrayList<CuentaBancaria> getCuentaBancaria(){
+        return this.cuentas;
     }
     
     public boolean agregarEmpleado(Empleado p_empleado) {
@@ -83,5 +100,96 @@ public class Banco{
         this.listarSueldos();
         System.out.println("\n");
         System.out.println("Total a Pagar-----------------------------------"+this.sueldoAPagar());
+    }
+    
+    //Ejercicio 4
+    
+    public boolean agregarCuentaBancaria(CuentaBancaria p_cuenta){
+       return this.cuentas.add(p_cuenta);
+    }
+    
+    public boolean quitarCuentaBancaria(CuentaBancaria p_cuenta){
+         if(this.getCuentaBancaria().size() > 1){
+            return this.getCuentaBancaria().remove(p_cuenta); 
+        } else { 
+            return false;
+        }
+    }
+    
+    public void listarCuentasConSaldoCero(){
+        System.out.println("Cuentas sin saldo:");
+        System.out.println("--- Cuenta -------------------- Apellido y Nombre -----------");
+    
+        if (this.getCuentaBancaria() != null) {
+            for(CuentaBancaria cuenta : this.getCuentaBancaria()){
+                if(cuenta.getSaldo() == 0){
+                    // Usamos apeYNom() para obtener "Apellido, Nombre"
+                    System.out.println(cuenta.getNroCuanta() + "\t" + cuenta.getTitular().apeYNom());
+                }
+            }
+        }
+    }
+    
+    public HashSet<Persona> listaDeTitulares(){
+    HashSet<Persona> titularesUnicos = new HashSet<Persona>();
+    
+    // Verificamos que la lista de cuentas no sea null
+    if (this.getCuentaBancaria() != null) {
+        for (CuentaBancaria cuenta : this.getCuentaBancaria()) {
+            titularesUnicos.add(cuenta.getTitular());
+        }
+    }
+    
+    // Mostrar el listado formateado
+    System.out.print("Listado de Clientes: ");
+    
+    int contador = 0;
+    for (Persona titular : titularesUnicos) {
+        System.out.print(titular.apeYNom());
+        contador++;
+        if (contador < titularesUnicos.size()) {
+            System.out.print("; ");
+        }
+    }
+    System.out.println(); // Salto de línea final
+    
+    return titularesUnicos;
+    }
+    
+    private int cuentasSaldoActivo(){
+        int contador = 0;
+    // Verificamos que la lista de cuentas no sea null
+        if (this.cuentas != null) {
+            for (CuentaBancaria cuenta : this.cuentas) {
+                // Contamos las cuentas con saldo mayor a 0
+                if (cuenta.getSaldo() > 0) {
+                    contador++;
+                }
+            }
+        }
+    
+        return contador;
+    }
+    
+    public void mostrarResumen(){
+        System.out.println("Banco: "+this.getNombre()+"\tSucursal: "+this.getNroSucursal());
+        System.out.println(this.getLocalidad().mostrar());
+        System.out.println("************************************************************\nRESUMEN DE CUENTAS BANCARIAS\n************************************************************");
+        System.out.println("Numero total de Cuentas Bancarias: "+this.cuentas.size());
+        System.out.println("Cuentas Activas: "+this.cuentasSaldoActivo());
+        int contador = 0;
+        if (this.cuentas != null) {
+            for (CuentaBancaria cuenta : this.getCuentaBancaria()) {
+                if (cuenta.getSaldo() == 0) {
+                    contador++;
+                }
+            }
+        }
+        System.out.println("Cuentas con Saldo Cero: "+contador);
+        System.out.println("-------------------------------------------------------------");
+        this.listarCuentasConSaldoCero();
+        System.out.println("-------------------------------------------------------------");
+        this.listaDeTitulares();
+        System.out.println("-------------------------------------------------------------");
     }
 }
